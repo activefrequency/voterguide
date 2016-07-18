442682a863e00a60a4c15248164543bd6cbbb0fa
Kevin Grinberg <kevin@activefrequency.com>
2016-07-18 16:54:49 -0400

    Include statewide candidates in local results

--- voterguide/views.py
+++ voterguide/views.py
@@ -116,6 +116,9 @@ def district_lookup(request):
     if city:
         conditions = Q(conditions | Q(race__district__city=city))
 
+    # show statewide candidates as well
+    conditions = Q(conditions | Q(race__district__isnull=True))
+
     candidates = Candidate.objects.filter(race__election=current_election).filter(conditions).distinct().select_related('person', 'race', 'race__office', 'race__district').order_by(
         'race__state', 'race__election', 'race__office', 'race__district', '-is_incumbent', 'person__last_name', 'person__first_name')
 
